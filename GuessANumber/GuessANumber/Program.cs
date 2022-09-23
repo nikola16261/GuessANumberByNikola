09dd9d6b7eca2502cab64d44d549030a6b2e7b8c
Nikola Chonov <nikolachonov@Nikolas-MacBook-Air.local>
2022-09-23 21:19:38 +0300

    GuessANumber

---
+++ GuessANumber/GuessANumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+            while (true)
+            {
+                Console.WriteLine("Guess a number (1-100): ");
+
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNumber > computerNumber)
+                    {
+                        Console.WriteLine("Too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+    }
+}
+
